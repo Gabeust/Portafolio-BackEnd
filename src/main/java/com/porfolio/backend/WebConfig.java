12f43894a5cdcb8666612b217f530bfe35ca0809
Gabriel Romero <romero.gabriel.e@hotmail.com>
2023-05-12 08:26:11 -0300

    actualizacion cors

---
+++ src/main/java/com/porfolio/backend/WebConfig.java
@@ -0,0 +1,21 @@
+package com.porfolio.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://gabrielromeroapp.web.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
